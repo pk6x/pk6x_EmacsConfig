2922ab46f6f793f7277f233029d9366a683d60fc
pk6x <a.mgundam78@gmail.com>
2023-09-30 00:08:02 +0100

    added more keybinds

--- init.el
+++ init.el
@@ -391,6 +391,8 @@
 (define-key global-map [f9] 'first-error)
 (define-key global-map [f10] 'previous-error)
 (define-key global-map [f11] 'next-error)
+(define-key global-map "\en" 'next-error)
+(define-key global-map "\eN" 'previous-error)
 (define-key global-map "\eg" 'goto-line)
 (define-key global-map "\ej" 'imenu)
 ;; (define-key global-map "^Q" 'copy-region-as-kill)
@@ -423,7 +425,9 @@
 (define-key global-map [pgdown] 'backward-page)
 (define-key global-map [C-next] 'scroll-other-window)
 (define-key global-map [C-prior] 'scroll-other-window-down)
-
+(define-key global-map "\ew" 'other-window)
+(define-key global-map "\ec" 'kill-ring-save)
+(define-key global-map "\ep" 'yank)
 ;; //////////////////////////////////////// End of Customised keybinds
 
 ;; //////////////////////////////////////// External packages
@@ -434,6 +438,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(auto-save-default nil)
+ '(auto-save-interval 0)
+ '(auto-save-list-file-prefix nil)
+ '(auto-save-timeout 0)
+ '(auto-show-mode t t)
+ '(delete-auto-save-files nil)
+ '(delete-old-versions (quote other))
+ '(imenu-auto-rescan t)
+ '(imenu-auto-rescan-maxout 500000)
+ '(kept-new-versions 5)
+ '(kept-old-versions 5)
+ '(make-backup-file-name-function (quote ignore))
+ '(make-backup-files nil)
+ '(mosue-wheel-follow-mouse nil)
+ '(mouse-wheel-progressive-speed nil)
+ '(mouse-wheel-scroll-amount (quote (15)))
+ '(version-control nil)
  '(custom-enabled-themes '(tsdh-dark))
  '(custom-safe-themes
    '("f366d4bc6d14dcac2963d45df51956b2409a15b770ec2f6d730e73ce0ca5c8a7" "1c7635fd451cc7834a4ec6ff92bf656d846cf75d9153ff01618f0d3e80323f04" default))
