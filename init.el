37158797071bd8d57fa61cd6b66bab7de27b7e44
pk6x <a.mgundam78@gmail.com>
2024-10-21 22:57:57 +0100

    Updated

--- init.el
+++ init.el
@@ -619,7 +619,7 @@
      (defun copy-word (&optional arg)
       "Copy words at point into kill-ring"
        (interactive "P")
-       (copy-thing 'backward-word 'forward-word arg)
+       (copy-thing 'forward-word 'backward-word arg)
        ;;(paste-to-mark arg)
        )
 
